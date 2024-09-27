c5ab87d3a4eb57575c8b7bc9c5c18a2a519ea567
nada7harby <87335429+nada7harby@users.noreply.github.com>
2024-09-27 22:14:54 +0300

    contact

--- src/Components/FormContact/ContactHome.jsx
+++ src/Components/FormContact/ContactHome.jsx
@@ -5,27 +5,51 @@ function ContactHome() {
   return (
     <>
       <Container fluid>
-        <Row className="m-4 pt-5 ContactHome">
-          <Col md={6} sm={7} xs={10}>
-            <div>
-              <Form.Group className="mb-3">
-                <Form.Label>Disabled input</Form.Label>
-                <Form.Control placeholder="Disabled input" />
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Label>Disabled select menu</Form.Label>
-                <Form.Select>
-                  <option>Disabled select</option>
-                  <option>Disabled select</option>
-                  <option>Disabled select</option>
-                </Form.Select>
-              </Form.Group>
-              <Form.Group className="mb-3">
-                <Form.Check type="checkbox" label="Can't check this" />
-              </Form.Group>
+        <Row className="m-4 pt-5 pb-5 ContactHome">
+          <Col md={4} sm={7} xs={11}>
+            <div className="containForm">
+              <div className="FormContactHome">
+                <h2>Book Appointment</h2>
+                <Form.Group className="mb-4">
+                  <Form.Select>
+                    <option value="" disabled selected hidden>
+                      Select a Department
+                    </option>
+                    <option>Cardiology</option>
+                    <option>Neurology</option>
+                    <option>Orthopedics</option>
+                    <option>Dermatology</option>
+                    <option>Pediatrics</option>
+                    <option>Radiology</option>
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-4">
+                  <Form.Select>
+                    <option value="" disabled selected hidden>
+                      Select a Doctor
+                    </option>
+                    <option>Ahmed</option>
+                    <option>Nour</option>
+                    <option>Iyad</option>
+                    <option>Moagmed</option>
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-4">
+                  <Form.Control placeholder="Your Name" />
+                </Form.Group>
+                <Form.Group className="mb-4">
+                  <Form.Control placeholder="your Phone" />
+                </Form.Group>
+                <Form.Group className="mb-4">
+                  <Form.Control placeholder="dd/mm/yy" type="date" />
+                </Form.Group>
+                <div className="text-center">
+                  <button className="btn">Appointment Now</button>
+                </div>
+              </div>
             </div>
           </Col>
-          <Col md={5}  sm={7} xs={10}>
+          <Col md={5} sm={7} xs={10}>
             <div>
               <img src={formimg} />
             </div>
